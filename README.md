1ad09db21ef265b5117cc2822ef45df5e452ba76
Peter <peter@zeal.global>
2020-02-05 14:05:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # wordpress-papercite-pages
 Automatically generate virtual subpages for papercite entries
+
+## How to use
+
+The following todo is VERY important as this plugin uses specifically named pages. Yes this is very limiting, but submit a PR with an admin interface and we can fix this.
+
+Make a page with the slug "publications" and on that page put you full bibliography shortcode (and anything else you want on your publication page)
+
+i.e. 
+
+```
+[bibfilter format="ieee_link" group=year group_order=desc author="Casper the Ghost|Richard Burton|John Legend" allow=incollection,mastersthesis,article,book,inbook,techreport,misc sortauthors=1]
+```
+
+Then create a page with the slug "publication" and make sure its parent page is the previously made "publications" so the full permalink is "publications/publication"
+
+on this page put the following shortcode
+
+```
+[singletex]
+```
+
+## Samples
+
+* https://zeal.global/publications
---
+++ papercite-pages.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Plugin Name: Pages for Papercite
+ * Plugin URI: https://zeal.global/projects
+ * Description: Automatic pages for papercite
+ * Version: 1.0
+ * Author: zeal
+ * Author URI: https://zeal.global
+ */
+
+
+ 
+function papercite_pages_ispublicationpage() {
+    global $post;
+    static $ispublication;
+    if ( empty( $ispublication ) ) {
+        $ispublication = false;
+
+        if ( strtolower($post->post_title) == "publication" && preg_match( '#\[singletex.*\]#', $post->post_content, $matches ) === 1 ) {
+            $ispublication = true;
+        }
+    }
+    return $ispublication;
+}
+
+function papercite_pages_publication_title() {
+    static $title;
+    if ( empty( $title ) ) {
+        if (papercite_pages_ispublicationpage()) {
+            $pub_name = preg_quote(urldecode(get_query_var( 'pub_name' )), "/");
+            if($pub_name != "") {
+                $pubtitle = trim (strip_tags ( apply_filters( 'the_content', '[bibtex filter:urltitle="/^' . $pub_name . '$/mi" bibtex_template="title-bibtex"]', "")));
+                //$pubtitle = "none";
+                if ($pubtitle != "") {
+                    $title = $pubtitle;
+                } else {
+                    $title = "[NOTFOUND]";
+                }
+            } else {
+                $title = "[NONE]";
+            }  
+        } else {
+            $title = "[NOTPUB]";
+        }
+    }
+    return $title;
+}
+
+function papercite_pages_publication_redirect() {
+    if (papercite_pages_ispublicationpage()) {
+        $title = papercite_pages_publication_title();   
+        if ($title == "[NONE]" || $title == "[NOTFOUND]") {
+            wp_redirect( home_url( '/publications/' ) );
+            die;  
+        }
+    }
+}
+add_action( 'template_redirect', 'papercite_pages_redirect' );
+
+function papercite_pages_document_title_parts ( $title ) {  
+    global $post;
+    if ( ! $post || ! $post->post_content || !is_page() ) {
+        return $title;
+    }
+    if (papercite_pages_ispublicationpage()) {
+        $title['title'] = papercite_pages_publication_title();     
+    }
+    return  $title;
+}
+add_filter('document_title_parts', 'papercite_pages_document_title_parts');
+
+function papercite_pages_page_title($title) {
+    global $post;
+    if ( ! $post || ! $post->post_content || !is_page() ) {
+        return $title;
+    }
+    if (strtolower($title) == "publication" && papercite_pages_ispublicationpage()) {
+        return papercite_pages_publication_title();  
+    }
+    return $title;
+}
+add_filter('the_title', 'papercite_pages_page_title', 99);
+
+function papercite_pages_query_vars($aVars) {
+    $aVars[] = "pub_name"; // represents the name of the product category as shown in the URL
+    return $aVars;
+}     
+add_filter('query_vars', 'papercite_pages_query_vars');
+
+function papercite_pages_singletex($attr,$content){
+    $pub_name = preg_quote(urldecode(get_query_var( 'pub_name' )), "/");
+    if($pub_name != "") {
+        return apply_filters( 'the_content', '[bibtex format="ieee" filter:urltitle="/^' . $pub_name . '$/mi" bibtex_template="item-bibtex"]', "");
+        //return "test";
+    } else {
+        return "NO PUB";
+    }
+}
+add_shortcode("singletex","papercite_pages_singletex");
+
+
+function papercite_pages_init() {
+    function add_rewrite_rules($aRules) {
+        $aNewRules = array('publications/([^/]+)/?$' => 'index.php?pagename=publications/publication&pub_name=$matches[1]');
+        $aRules = $aNewRules + $aRules;
+        return $aRules;
+    }        
+    add_filter('rewrite_rules_array', 'add_rewrite_rules', 'top');
+}
+add_action('init', 'papercite_pages_init');
